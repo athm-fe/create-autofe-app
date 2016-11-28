0a0afd5f3b46b614cf3b61da173d14d6d8e856bb
jpuncle <jack_zhxk@163.com>
2016-11-28 11:22:11 +0800

    css文件在开发模式下支持sourcemaps

--- packages/autofe-scripts/gulpfile.js/tasks/sass.js
+++ packages/autofe-scripts/gulpfile.js/tasks/sass.js
@@ -6,16 +6,19 @@ var postcss = require('gulp-postcss');
 var assets  = require('postcss-assets');
 var autoprefixer = require('autoprefixer');
 var clean = require('gulp-clean-css');
+var sourcemaps = require('gulp-sourcemaps');
 var gulpif = require('gulp-if');
 var gutil = require('gulp-util');
 
 var sassTask = function () {
   return gulp.src(config.sass.src)
+    .pipe(gulpif(process.env.NODE_ENV !== 'production', sourcemaps.init()))
     .pipe(sass(config.sass.option).on('error', sass.logError))
     .pipe(postcss([
       assets(config.postcssAssets.option),
       autoprefixer(config.autoprefixer.option)
     ]))
+    .pipe(gulpif(process.env.NODE_ENV !== 'production', sourcemaps.write('./maps')))
     .pipe(gulpif(process.env.NODE_ENV === 'production', clean({ compatibility: 'ie7' })))
     .pipe(gulp.dest(config.sass.dest))
     .pipe(browserSync.stream());
