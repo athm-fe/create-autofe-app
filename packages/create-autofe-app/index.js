eefeda0bb091c671b6f726c432cf76c8473b2d7b
jpuncle <jack_zhxk@163.com>
2017-06-30 12:35:45 +0800

    重构 create-autofe-app 1. 使用 commander 替代 minimist，并提供更好的命令行使用体验 2. 去掉 path-exists ，使用 fs-extra 3. 使用 es6 code

--- packages/create-autofe-app/index.js
+++ packages/create-autofe-app/index.js
@@ -9,68 +9,109 @@
  * of patent rights can be found in the PATENTS file in the same directory.
  */
 
-'use strict';
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//   /!\ DO NOT MODIFY THIS FILE /!\
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//
+// create-react-app is installed globally on people's computers. This means
+// that it is extremely difficult to have them upgrade the version and
+// because there's only one global version installed, it is very prone to
+// breaking changes.
+//
+// The only job of create-react-app is to init the repository and then
+// forward all the commands to the local version of create-react-app.
+//
+// If you need to add a new command, please add it to the scripts/ folder.
+//
+// The only reason to modify this file is to add more warnings and
+// troubleshooting information for the `create-react-app` command.
+//
+// Do not make breaking changes! We absolutely don't want to have to
+// tell people to update their global version of create-react-app.
+//
+// Also be careful with new language features.
+// This file must work on Node 0.10+.
+//
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//   /!\ DO NOT MODIFY THIS FILE /!\
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-var fs = require('fs');
-var path = require('path');
-var spawn = require('cross-spawn');
-var chalk = require('chalk');
-var semver = require('semver');
-var argv = require('minimist')(process.argv.slice(2));
-var pathExists = require('path-exists');
+const chalk = require('chalk');
+const commander = require('commander');
+const fs = require('fs-extra');
+const path = require('path');
+const execSync = require('child_process').execSync;
+const spawn = require('cross-spawn');
+const semver = require('semver');
 
-/**
- * Arguments:
- *   --version - to print current version
- *   --verbose - to print logs while init
- *   --scripts-version <alternative package>
- *     Example of valid values:
- *     - a specific npm version: "0.2.0"
- *     - a .tgz archive from any npm repo: "https://registry.npmjs.org/autofe-scripts/-/autofe-scripts-0.2.0.tgz"
- *     - a package prepared with `tasks/clean_pack.sh`: "/Users/home/jpuncle/create-autofe-app/autofe-scripts-0.2.0.tgz"
- */
-var commands = argv._;
-if (commands.length === 0) {
-  if (argv.version) {
-    console.log('create-autofe-app version: ' + require('./package.json').version);
-    process.exit();
-  }
-  console.error(
-    'Usage: create-autofe-app <project-directory> [--verbose]'
-  );
+const packageInfo = require('./package.json');
+
+let projectName;
+
+const program = new commander.Command(packageInfo.name)
+  .version(packageInfo.version)
+  .arguments('<project-directory>')
+  .usage(`${chalk.green('<project-directory>')} [options]`)
+  .action((name) => {
+    projectName = name;
+  })
+  .option(
+    '--verbose',
+    'print additional logs')
+  .option(
+    '--scripts-version <alternative-package>',
+    'use a non-standard version of autofe-scripts')
+  .allowUnknownOption()
+  .on('--help', () => {
+    console.log(`    Only ${chalk.green('<project-directory>')} is required.`);
+    console.log();
+    console.log(`    A custom ${chalk.cyan('--scripts-version')} can be one of:`);
+    console.log(`      - a specific npm version: ${chalk.green('0.8.2')}`);
+    console.log(`      - a custom fork published on npm: ${chalk.green('my-autofe-scripts')}`);
+    console.log(`      - a .tgz archive: ${chalk.green('https://mysite.com/my-autofe-scripts-0.8.2.tgz')}`);
+    console.log('    It is not needed unless you specifically want to use a fork.');
+    console.log();
+    console.log('    If you have any problems, do not hesitate to file an issue:');
+    console.log(`      ${chalk.cyan('https://github.com/athm-fe/create-autofe-app/issues/new')}`);
+    console.log();
+  })
+  .parse(process.argv);
+
+if (typeof projectName === 'undefined') {
+  console.error('Please specify the project directory:');
+  console.log(`  ${chalk.cyan(program.name())} ${chalk.green('<project-directory>')}`);
+  console.log();
+  console.log('For example:');
+  console.log(`  ${chalk.cyan(program.name())} ${chalk.green('my-app')}`);
+  console.log();
+  console.log(`Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`);
   process.exit(1);
 }
 
-createApp(commands[0], argv.verbose, argv['scripts-version']);
+createApp(projectName, program.verbose, program.scriptsVersion);
 
 function createApp(name, verbose, version) {
-  var root = path.resolve(name);
-  var appName = path.basename(root);
+  const root = path.resolve(name);
+  const appName = path.basename(root);
 
   checkAppName(appName);
-
-  if (!pathExists.sync(name)) {
-    fs.mkdirSync(root);
-  } else if (!isSafeToCreateProjectIn(root)) {
-    console.log('The directory `' + name + '` contains file(s) that could conflict. Aborting.');
+  fs.ensureDirSync(name);
+  if (!isSafeToCreateProjectIn(root)) {
+    console.log(`The directory ${chalk.green(name)} contains files that could conflict.`);
+    console.log('Try using a new directory name.');
     process.exit(1);
   }
 
-  console.log(
-    'Creating a new AutoFE app in ' + root + '.'
-  );
+  console.log(`Creating a new AutoFE app in ${chalk.green(root)}.`);
   console.log();
 
-  var packageJson = {
+  const packageJson = {
     name: appName,
     version: '0.1.0',
     private: true,
   };
-  fs.writeFileSync(
-    path.join(root, 'package.json'),
-    JSON.stringify(packageJson, null, 2)
-  );
-  var originalDirectory = process.cwd();
+  fs.writeFileSync(path.join(root, 'package.json'), JSON.stringify(packageJson, null, 2));
+  const originalDirectory = process.cwd();
   process.chdir(root);
 
   console.log('Installing packages. This might take a couple minutes.');
@@ -80,70 +121,78 @@ function createApp(name, verbose, version) {
   run(root, appName, version, verbose, originalDirectory);
 }
 
-function install(packageToInstall, verbose, callback) {
-  var args = [
-    'add',
-    '--dev',
-    '--exact',
-    packageToInstall
-  ];
-  var proc = spawn('yarn', args, {stdio: 'inherit'});
+function shouldUseYarn() {
+  try {
+    execSync('yarn --version', { stdio: 'ignore' });
+    return true;
+  } catch (e) {
+    return false;
+  }
+}
 
-  var yarnExists = true;
-  proc.on('error', function (err) {
-    if (err.code === 'ENOENT') {
-      yarnExists = false;
-    }
-  });
-  proc.on('close', function (code) {
-    if (yarnExists) {
-      callback(code, 'yarn', args);
-      return;
-    }
-    // No Yarn installed, continuing with npm.
+function install(packageToInstall, verbose, callback) {
+  const useYarn = shouldUseYarn();
+  let command;
+  let args;
+  if (useYarn) {
+    command = 'yarn';
+    args = [
+      'add',
+      '--dev',
+      '--exact',
+      packageToInstall,
+    ];
+  } else {
+    command = 'npm';
     args = [
       'install',
-      verbose && '--verbose',
       '--save-dev',
       '--save-exact',
-      packageToInstall
-    ].filter(function(e) { return e; });
-    var npmProc = spawn('npm', args, {stdio: 'inherit'});
-    npmProc.on('close', function (code) {
-      callback(code, 'npm', args);
-    });
+      '--loglevel',
+      'error',
+      packageToInstall,
+    ];
+  }
+
+  if (verbose) {
+    args.push('--verbose');
+  }
+
+  const child = spawn(command, args, { stdio: 'inherit' });
+  child.on('close', (code) => {
+    callback(code, command, args);
   });
 }
 
 function run(root, appName, version, verbose, originalDirectory) {
-  var packageToInstall = getInstallPackage(version);
-  var packageName = getPackageName(packageToInstall);
+  const packageToInstall = getInstallPackage(version);
+  const packageName = getPackageName(packageToInstall);
 
-  install(packageToInstall, verbose, function (code, command, args) {
+  install(packageToInstall, verbose, (code, command, args) => {
     if (code !== 0) {
-      console.error('`' + command + ' ' + args.join(' ') + '` failed');
+      console.error(`${command} ${args.join(' ')} failed`);
       return;
     }
 
     checkNodeVersion(packageName);
 
-    var scriptsPath = path.resolve(
+    const scriptsPath = path.resolve(
       process.cwd(),
       'node_modules',
       packageName,
       'scripts',
-      'init.js'
+      'init.js',
     );
-    var init = require(scriptsPath);
+    const init = require(scriptsPath);
     init(root, appName, verbose, originalDirectory);
   });
 }
 
 function getInstallPackage(version) {
-  var packageToInstall = 'autofe-scripts';
-  var validSemver = semver.valid(version);
+  let packageToInstall = 'autofe-scripts';
+  const validSemver = semver.valid(version);
   if (validSemver) {
-    packageToInstall += '@' + validSemver;
+    packageToInstall += `@${validSemver}`;
   } else if (version) {
     // for tar.gz or alternative paths
     packageToInstall = version;
@@ -154,7 +203,8 @@ function getInstallPackage(version) {
 // Extract package name from tarball url or path.
 function getPackageName(installPackage) {
   if (installPackage.indexOf('.tgz') > -1) {
-    // The package name could be with or without semver version, e.g. autofe-scripts-0.2.0-alpha.1.tgz
+    // The package name could be with or without semver version,
+    // e.g. autofe-scripts-0.2.0-alpha.1.tgz
     // However, this function returns package name only wihout semver version.
     return installPackage.match(/^.+\/(.+?)(?:-\d+.+)?\.tgz$/)[1];
   } else if (installPackage.indexOf('@') > 0) {
@@ -165,13 +215,13 @@ function getPackageName(installPackage) {
 }
 
 function checkNodeVersion(packageName) {
-  var packageJsonPath = path.resolve(
+  const packageJsonPath = path.resolve(
     process.cwd(),
     'node_modules',
     packageName,
-    'package.json'
+    'package.json',
   );
-  var packageJson = require(packageJsonPath);
+  const packageJson = require(packageJsonPath);
   if (!packageJson.engines || !packageJson.engines.node) {
     return;
   }
@@ -179,11 +229,11 @@ function checkNodeVersion(packageName) {
   if (!semver.satisfies(process.version, packageJson.engines.node)) {
     console.error(
       chalk.red(
-        'You are currently running Node %s but create-autofe-app requires %s.' +
-        ' Please use a supported version of Node.\n'
-      ),
+        'You are running Node %s.\n' +
+          'Create AutoFE App requires Node %s or higher. \n' +
+          'Please update your version of Node.'),
       process.version,
-      packageJson.engines.node
+      packageJson.engines.node,
     );
     process.exit(1);
   }
@@ -191,23 +241,18 @@ function checkNodeVersion(packageName) {
 
 function checkAppName(appName) {
   // TODO: there should be a single place that holds the dependencies
-  var dependencies = [];
-  var devDependencies = ['autofe-scripts'];
-  var allDependencies = dependencies.concat(devDependencies).sort();
+  const dependencies = [];
+  const devDependencies = ['autofe-scripts'];
+  const allDependencies = dependencies.concat(devDependencies).sort();
 
   if (allDependencies.indexOf(appName) >= 0) {
     console.error(
       chalk.red(
-        'We cannot create a project called `' + appName + '` because a dependency with the same name exists.\n' +
-        'Due to the way npm works, the following names are not allowed:\n\n'
-      ) +
-      chalk.cyan(
-        allDependencies.map(function(depName) {
-          return '  ' + depName;
-        }).join('\n')
-      ) +
-      chalk.red('\n\nPlease choose a different project name.')
-    );
+        `We cannot create a project called ${chalk.green(appName)}
+        because a dependency with the same name exists.\n
+        Due to the way npm works, the following names are not allowed:\n\n`) +
+      chalk.cyan(allDependencies.map(depName => `  ${depName}`).join('\n')) +
+      chalk.red('\n\nPlease choose a different project name.'));
     process.exit(1);
   }
 }
@@ -216,11 +261,18 @@ function checkAppName(appName) {
 // We also special case IJ-based products .idea because it integrates with CRA:
 // https://github.com/facebookincubator/create-react-app/pull/368#issuecomment-243446094
 function isSafeToCreateProjectIn(root) {
-  var validFiles = [
-    '.DS_Store', 'Thumbs.db', '.git', '.gitignore', '.idea', 'README.md', 'LICENSE'
+  const validFiles = [
+    '.DS_Store',
+    'Thumbs.db',
+    '.git',
+    '.gitignore',
+    '.idea',
+    'README.md',
+    'LICENSE',
+    'web.iml',
+    '.hg',
+    '.hgignore',
+    '.hgcheck',
   ];
-  return fs.readdirSync(root)
-    .every(function(file) {
-      return validFiles.indexOf(file) >= 0;
-    });
+  return fs.readdirSync(root).every(file => validFiles.indexOf(file) >= 0);
 }
