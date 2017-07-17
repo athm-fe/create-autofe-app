856e63143520fa1fa700494a19860c5da5e4032e
jpuncle <jack_zhxk@163.com>
2017-07-17 17:18:38 +0800

    use babel to compile js

--- packages/autofe-scripts/config/webpack.config.js
+++ packages/autofe-scripts/config/webpack.config.js
@@ -30,4 +30,23 @@ module.exports = () => ({
     path: path.join(context, 'build'),
     publicPath: '/',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['env', { modules: false }],
+            ],
+            plugins: [
+              'syntax-dynamic-import',
+            ],
+          },
+        },
+      },
+    ],
+  },
 });
