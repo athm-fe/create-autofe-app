8177a57da47f03acd9ec7ad942ed0b730dc0eef4
jpuncle <jack_zhxk@163.com>
2017-08-08 15:33:07 +0800

    调整es6示例代码的顺序

--- packages/autofe-scripts/template/src/index/js/index.entry.js
+++ packages/autofe-scripts/template/src/index/js/index.entry.js
@@ -1,8 +1,8 @@
 console.log('======== webpack + babel ========');
 
-import(/* webpackChunkName: "es5-code" */ './es5-code');
 import(/* webpackChunkName: "commonjs" */ './commonjs');
 import(/* webpackChunkName: "es6-modules" */ './es6-modules');
+import(/* webpackChunkName: "es5-code" */ './es5-code');
 import(/* webpackChunkName: "es6-code" */ './es6-code');
 import(/* webpackChunkName: "es2016plus" */ './es2016plus');
 import(/* webpackChunkName: "stage" */ './stage');
