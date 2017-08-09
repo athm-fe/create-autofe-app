50e92c07068c87b6f0c99ecf3a6b7e4607415cbc
jpuncle <jack_zhxk@163.com>
2017-08-09 17:56:16 +0800

    autofe-polyfill: add todos

--- packages/autofe-polyfill/README.md
+++ packages/autofe-polyfill/README.md
@@ -1,7 +1,3 @@
 # autofe-polyfill
 
 This package inclues polyfills used by [Create AutoFE App](https://github.com/athm-fe/create-autofe-app).
-Please refer to its documentation:
-
-* [Getting Started](https://github.com/athm-fe/create-autofe-app/blob/master/README.md#getting-started) – How to create a new app.
-* [User Guide](https://github.com/athm-fe/create-autofe-app/blob/master/packages/autofe-scripts/template/README.md) – How to develop apps bootstrapped with Create React App.
--- packages/autofe-polyfill/index.js
+++ packages/autofe-polyfill/index.js
@@ -1,49 +1,40 @@
-// JSON?
-// 额外引入吧，这里不管
-// JSON3 or JSON2
-
-// es5-shim
+// es5-shim or get from core-js
 // https://github.com/es-shims/es5-shim#shims
+// import()/require.ensure()
+Function.prototype.bind();
+// Object rest
+Array.prototype.indexOf();
+// string spread
+// string destructuring
+Array.isArray();
+
 
 // es5-sham or get from core-js
 // https://github.com/es-shims/es5-shim#shams
 // 只要下面这一个，其它的不要
 // class
 Object.create();
-
-// Object spread
-Object.assign();
-
-// Object rest
-Array.prototype.indexOf();
-
-// import()/require.ensure()
-Function.prototype.bind();
-
-// import()/require.ensure()
-// es6-promise
-console.log(new Promise());
-
-// webpackBootstrap
-console.error();
-
-// string spread
-Array.isArray();
-
-// string spread
-Array.from();
-
-// string destructuring
-// _slicedToArray helper
-Array.isArray();
-// Symbol.iterator
-// arr[Symbol.iterator]()
-
 // object enhanced, super.xxx()
 // _get helper
-// 是否有 loose 模式？
 Object.getOwnPropertyDescriptor();
+// Object.defineProperty
+// Object.defineProperties
+// Object.getPrototypeOf
+// Object.freeze
 
-// Symbols
-// [Symbol.iterator]()
-// Iterators
+
+// es6
+// string destructuring
+// _slicedToArray helper
+// Symbol
+// Iterator
+[][Symbol.iterator]();
+// import()/require.ensure()
+// es6-promise
+Promise.all();
+// string spread
+Array.from();
+// Object spread
+Object.assign();
+// Object.setPrototypeOf
+// Object.prototype.__proto__
