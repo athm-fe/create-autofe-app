e9c8fcad32bee365f618c19babef85678dfb9fe5
jpuncle <jack_zhxk@163.com>
2017-08-10 19:22:18 +0800

    split index.entry.js

--- packages/autofe-scripts/template/src/index/js/index.entry.js
+++
@@ -1,9 +0,0 @@
-console.log('======== webpack + babel ========');
-
-require('./commonjs');
-require('./es5-code');
-require('./es6-code');
-require('./es2016plus');
-require('./stage');
-require('./es6-modules');
-require('./dynamic-import');
--- packages/autofe-scripts/template/src/index/js/main.old.js
+++ packages/autofe-scripts/template/src/index/js/main.old.js
@@ -1,3 +1,5 @@
 (function () {
   console.log('Hello world from old script.');
 }());
+
+console.log('-------- old script --------');
