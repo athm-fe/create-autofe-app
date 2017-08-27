2bf158a2cf4d9100f2cd2806ccaad3fb4373342f
jpuncle <jack_zhxk@163.com>
2017-08-27 11:37:49 +0800

    only use imagemin when prod, for build speed

--- packages/autofe-scripts/gulpfile.js/tasks/images.js
+++ packages/autofe-scripts/gulpfile.js/tasks/images.js
@@ -2,10 +2,14 @@ const gulp = require('gulp');
 const config = require('../config');
 const browserSync = require('../lib/browserSync');
 const imagemin = require('gulp-imagemin');
+const gulpif = require('gulp-if');
 
 const imagesTask = function () {
   return gulp.src(config.images.src)
-    .pipe(imagemin(config.images.imagemin))
+    .pipe(gulpif(
+      process.env.NODE_ENV === 'production',
+      imagemin(config.images.imagemin),
+    ))
     .pipe(gulp.dest(config.images.dest))
     .on('end', browserSync.reload);
 };
