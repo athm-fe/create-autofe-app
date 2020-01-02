552852d87f7432371bf950f6f3acc188925c473d
jpuncle <jp2uncle@gmail.com>
2020-01-02 14:56:09 +0800

    feat: update autofe-polyfill

--- packages/autofe-polyfill/index.js
+++ packages/autofe-polyfill/index.js
@@ -1,18 +1,8 @@
-require('es6-promise/auto');
-Object.assign = require('object-assign');
+var ES6Promise = require('es6-promise');
+var objectAssign = require('object-assign');
 
-// Symbol.iterator
-// Array.from()
+ES6Promise.polyfill();
 
-// Symbol.asyncIterator
-
-// promise polyfill alone doesn't work in IE,
-// needs this as well. see: #1642
-'es.array.iterator',
-// this is required for webpack code splitting, vuex etc.
-'es.promise',
-// this is needed for object rest spread support in templates
-// as vue-template-es2015-compiler 1.8+ compiles it to Object.assign() calls.
-'es.object.assign',
-// #2012 es6.promise replaces native Promise in FF and causes missing finally
-'es.promise.finally'
+if (typeof Object.assign != 'function') {
+  Object.assign = objectAssign;
+}
