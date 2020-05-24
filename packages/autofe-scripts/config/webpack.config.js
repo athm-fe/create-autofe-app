df2c9712647022e2773d45f92e3d4d28bae32f34
jpuncle <jack_zhxk@163.com>
2020-05-24 23:13:08 +0800

    feat: 去掉由于 src 没有 ts 文件导致的错误日志

--- packages/autofe-scripts/config/webpack.config.js
+++ packages/autofe-scripts/config/webpack.config.js
@@ -171,6 +171,20 @@ function getPrependDataForSassLoader(prependData = '') {
   return `${data}\n${prependData}`;
 }
 
+function getForkTsCheckerLogger() {
+  const myLogger = Object.create(console);
+  const reg = /No inputs were found in config file.+tsconfig\.json/;
+
+  myLogger.error = (msg, ...args) => {
+    if (typeof msg === 'string' && reg.test(msg)) {
+      return;
+    }
+    console.error(msg, ...args);
+  };
+
+  return myLogger;
+}
+
 module.exports = () => {
   const transpileDepRegex = genTranspileDepRegex(config.transpileDependencies);
 
@@ -623,6 +637,7 @@ module.exports = () => {
             resolveModule('typescript/package.json', context) ||
             resolveModule('typescript/package.json', __dirname)
           ),
+          logger: getForkTsCheckerLogger(),
           // vue: true,
           formatter: 'codeframe',
           checkSyntacticErrors: true,
